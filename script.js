bd8bdaa85dde72e7b5dff0b180fa81e341891774
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-11-03 17:55:12 +0530

    commit 13 on 01 11 2023

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function assignData(Data) {
   n = data.length;
 
   // Sort the data based on scores in descending order
-  data.sort((a, b) => b.score - a.score);
+  data.sort((a, b) => b.MACscore - a.MACscore);
   console.log("Data Sorting Success!!");
 
   // Assign ranks to students with the same score
   var rank = 1;
   for (i = 0; i < n; i++) {
-    if (i > 0 && data[i].score !== data[i - 1].score) {
+    if (i > 0 && data[i].MACscore !== data[i - 1].MACscore) {
       rank++;
     }
     data[i].rank = rank;
@@ -58,14 +58,14 @@ function assignData(Data) {
             }
       }
       top10Div.innerHTML = top10Div.innerHTML + `
-      <div class="col-5 col-md-3 rankItemTop text-center  d-flex flex-column justify-content-around align-items-center mx-2 firstRank">
+      <div class="col-8 col-md-3 rankItemTop text-center  d-flex flex-column justify-content-around align-items-center mx-2 firstRank">
             <div>
                 <img src="${rankImgSrc}" alt="" class="firstRankPic mt-3">
                 <p class=" d-block display-3 en-sirin  z-3 ${color}">#${data[i].rank}</p>
             </div>
             <div class="mt-4">
                 <p class="en-Oxanium h4 text-nowrap text-info">${data[i].studentName}</p>
-                <p class="en-Oxanium h4 topRankScore p-1 px-3">${data[i].score}</p>
+                <p class="en-Oxanium h4 topRankScore p-1 px-3">${data[i].MACscore}</p>
             </div>
         </div>
       
@@ -73,18 +73,44 @@ function assignData(Data) {
     }
 
     else{
+      switch(data[i].rank){
+          case 1: 
+          {
+              rankImgSrc = "assets/img/firstCrown.png"; color = "text-warning";
+              break;
+              
+          }
+          case 2:
+            {
+              rankImgSrc = "assets/img/secondCrown.png"; color = "text-silver";
+              break;
+              
+            }
+          case 3:
+            {
+              rankImgSrc = "assets/img/thirdCrown.png"; 
+              color = "text-bronze";
+              break;   
+            }
+          default: 
+            {
+                rankImgSrc = "assets/img/defaultRank.png";
+                color = "text-info";
+            }
+        }
           rankList.innerHTML = rankList.innerHTML + 
           `
             <div class="text-light normalRankItem d-flex flex-row justify-content-around align-items-center normalRankBg px-2 py-1">
-            <p class="rank my-auto h1 en-iceberg p-0 m-0">#${data[i].rank}</p>
+            <p class="rank my-auto h3 en-iceberg p-0 m-0">#${data[i].rank}</p>
             <img src="assets/img/defaultRank.png" class="normalRankPic"></img>
             <p class="normalRankName my-auto text-nowrap">${data[i].studentName}</p>
-            <p class="normalRankScore my-auto">${data[i].score}</p>
+            <p class="normalRankScore my-auto">${data[i].MACscore}</p>
           </div>
           `
     }
   }
 }
+
 
 // old rank List item
 // `
