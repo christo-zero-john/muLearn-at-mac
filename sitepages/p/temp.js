dc7a3ac39e39bd637fac3faff19846319b7c4b1d
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-12-14 18:01:59 +0530

    commit 79 on 14 12 2023

--- sitepages/p/temp.js
+++
@@ -1,4 +0,0 @@
-if(!getStudentById(addData[addData.length-1].completed.includes(newData.completed))){
-    addData[addData.length-1].completed.push(newData.completed)
-}
-console.log(!getStudentById(addData[addData.length-1].completed.includes(newData.completed)))
