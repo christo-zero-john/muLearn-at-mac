b3d2fce5b84a9bcbc3a999d084d635f955f2762c
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2024-01-06 11:01:59 +0530

    commit on 11 01 2024

---
+++ p/execomtemp.md
@@ -0,0 +1,40 @@
+https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+https://www.linkedin.com/in/sunil-k-joseph/"
+
+https://www.instagram.com/suniljosephk/"
+
+https://www.linkedin.com/in/christo-john-kalapurackal/"
+
+https://www.instagram.com/zero.christo.john/"
+
+https://www.linkedin.com/in/alwin-siby/"
+
+https://www.instagram.com/alwin_siby_/"
+
+https://www.linkedin.com/in/christo-john-kalapurackal/"
+
+https://www.instagram.com/zero.christo.john/"
+
+https://www.linkedin.com/in/christo-john-kalapurackal/"
+
+https://www.instagram.com/zero.christo.john/"
+
+https://www.linkedin.com/in/christo-john-kalapurackal/"
+
+https://www.instagram.com/zero.christo.john/"
+
+<!-- binnet -->
+https://www.linkedin.com/in/binnet-binoy-47225126b/"
+
+https://www.instagram.com/binnet_binoy/"
+
+<!-- pavan -->
+https://www.instagram.com/_pa__vi__/"
+
+https://www.linkedin.com/in/pavan-t-sunu-46787b26a/"
+
+<!-- adithya  -->
+https://www.linkedin.com/in/adithya-viswambharan-b3a87b26a/"
+
+https://www.instagram.com/_.adi__thya_/"
+
--- scripts/execom.js
+++ scripts/execom.js
@@ -17,3 +17,70 @@ $(document).ready(function () {
 
 var execomDiv = document.getElementById("execomDiv");
 
+
+var spreadsheet_id = '1ROUxDLf3nZ_gAJjjjcO5yRgeeRGgncSCGUgCxBWBWVo'; //mulearn data google spreadsheet id
+
+fetch(`https://opensheet.elk.sh/${spreadsheet_id}/execom`)
+    .then(res => res.json())
+    .then((data) => {
+        printExecom(data);
+    })
+
+function printExecom(data){
+    console.log(data);
+    execomDiv.innerHTML = "";
+    for(x=0; x<2; x++){
+        execomDiv.innerHTML += `
+        <div class="col-11 mx-auto col-md-3 mx-md-2 mt-3">
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-front">
+                    <img src="${data[x].image}" class="card-img-top" alt="${data[x].title}">
+                    <p class="title pt-2 fs-4 text-danger text-center">${data[x].title}</p>
+                </div>
+                <div class="card-back">
+                    <p class="title pt-2 text-center text-greenyellow fs-3">${data[x].title}</p>
+                    <p class="card-text name fs-5 text-info fs-5">${data[x].name}</p>
+                    <p class="description">
+                    ${data[x].description}
+                    </p>
+                    <div class="social-icons text-nowrap text-center">
+                    
+                        <a href="https://www.linkedin.com/in/${data[x].linkedin}"><img src="/assets/img/linkedin.png" alt="LinkedIn" class="socialIcon btn col-3 m-0"></a>
+                        <a href="https://www.github.com/in/${data[x].github}"><img src="/assets/img/github.png" alt="GitHub" class="socialIcon btn col-3 m-0"></a>
+                        <a href="https://www.instagram.com/${data[x].instagram}/"><img src="/assets/img/instagram.png" alt="Instagram" class="socialIcon btn col-3 m-0"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    }
+
+    for(x=2; x<data.length; x++){
+        execomDiv.innerHTML += `
+        <div class="col-11 mx-auto col-md-3 mx-md-2 mt-3">
+        <div class="card">
+            <div class="card-inner">
+                <div class="card-front">
+                    <img src="${data[x].image}" class="card-img-top" alt="${data[x].title}">
+                    <p class="title pt-2 fs-4 text-danger text-center">${data[x].title}</p>
+                </div>
+                <div class="card-back">
+                    <p class="title pt-2 text-center text-greenyellow fs-3">${data[x].title}</p>
+                    <p class="card-text name fs-5 text-info fs-5">${data[x].name}</p>
+                    <p class="description">
+                    ${data[x].description}
+                    </p>
+                    <div class="social-icons text-nowrap text-center">
+                    
+                        <a href="https://www.linkedin.com/in/${data[x].linkedin}"><img src="/assets/img/linkedin.png" alt="LinkedIn" class="socialIcon btn">
+                        <a href="https://www.github.com/in/${data[x].github}"><img src="/assets/img/github.png" alt="LinkedIn" class="socialIcon btn col-3 m-0">
+                        </a>
+                        <a href="https://www.instagram.com/${data[x].instagram}/"><img src="/assets/img/instagram.png" alt="Instagram" class="socialIcon btn"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+    }
+}
